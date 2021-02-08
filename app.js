13c4689946df93390656646cbc5898be4363c16d
Cy <64352340+cyyap17@users.noreply.github.com>
2021-02-09 00:17:11 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+document.querySelector("img.exort").addEventListener("click",function(){
+  const invoker = new Audio("mp3/invoker.mp3");
+  invoker.play();
+  
+})
+document.querySelector("img.wex").addEventListener("click",function(){
+  const invoker2 = new Audio("mp3/invoker2.mp3");
+  invoker2.play();
+})
+document.querySelector("img.quas").addEventListener("click",function(){
+  const invoker3 = new Audio("mp3/invoker3.mp3");
+  invoker3.play();
+})
